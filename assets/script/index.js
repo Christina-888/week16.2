91aae6ad21151288f19c7baec8f29c35ebf96121
Christina-888 <christinabykadorova@gmail.com>
2025-03-09 15:12:25 +0300

    add features

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,6 +1,11 @@
 const brand = document.getElementById('brand');
 const model = document.getElementById('model');
 const result = document.getElementById('result');
+const volume = document.getElementById('volume');
+const newCar = document.getElementById('new');
+const used = document.getElementById('used');
+const oneTwo = document.getElementById('one-two');
+const three = document.getElementById('three');
 
 //Модели:
 const models = {
@@ -9,6 +14,27 @@ const models = {
   Haval: ['Jolion', 'Dargo'],
   Subaru: ['Forester', 'Stella'],
 };
+
+//Убираем поле владельцев, если машина новая:
+const ownersField = document.querySelector('.form__owners');
+const ownersRemove = () => {
+if (newCar.checked) {
+  ownersField.style.display = 'none';
+} else if (used.checked) {
+  ownersField.style.display = 'block';
+}
+};
+
+newCar.addEventListener('change', () => {
+  ownersRemove(); 
+  calculatePrice(); 
+});
+
+used.addEventListener('change', () => {
+  ownersRemove(); 
+  calculatePrice(); 
+});
+
 
 //Обработчик события change:
 model.addEventListener('change', function() {
@@ -38,8 +64,29 @@ const calculatePrice = () => {
   } else if (model.value === 'Jolion') {
     price += 1670000;
   } else if (model.value === 'Forester') {
-    prise += 330000;
+    price += 330000;
   };
+
+  //Изменение цены в завис. от V двигателя:
+const volumeNumber = parseFloat(volume.value);
+
+if (volumeNumber) {
+  price += volumeNumber * 10000;
+};
+
+//Изменение цены в завис. от состояния:
+if (used.checked) {
+  price -= 380000;
+}
+
+//Изменение цены в завис. от количества владельцев:
+if (oneTwo.checked) {
+  price -= 100000;
+};
+
+if(three.checked) {
+  price -= 350000;
+};
 
   result.textContent = `Итоговая стоимость авто: ${price} рублей.`
 };
